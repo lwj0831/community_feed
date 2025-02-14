221d8ecb7dfbf8a49a7caf9fc61481e91216106b
lwj0831 <wjl0831@gmail.com>
2025-02-14 19:15:51 +0900

    initial commit

---
+++ src/main/java/org/fastcampus/community_feed/CommunityFeedApplication.java
@@ -0,0 +1,13 @@
+package org.fastcampus.community_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fastcampus/community_feed/CommunityFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastcampus.community_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
